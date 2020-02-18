02ee39a41f8a2f9127ca5e6de80c26b8acf98847
memem <memem@DESKTOP-3FJF3RL>
2020-02-17 22:11:44 -0600

    Adding JUnitTests

--- WordSearchKata/src/WordSearchKata.java
+++ WordSearchKata/src/WordSearchKata.java
@@ -2,10 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class WordSearchKata {
-
+	
 	public static void main(String[] args) {
+		System.out.print(wordsearch(args));
+	}
+
+	public static String wordsearch(String[] args) {
 		//Parse file
-		String filename = "C:\\Users\\memem\\eclipse-workspace\\WordSearchKata\\src\\default2.txt";
+		java.io.ByteArrayOutputStream out = new java.io.ByteArrayOutputStream();    
+		System.setOut(new java.io.PrintStream(out)); 
+		String filename = "C:\\Users\\memem\\eclipse-workspace\\WordSearchKata\\src\\default.txt";
 		List<String> wordsearch = new ArrayList<String>();
 		String[] wordlist = null;
 		if(args.length > 0) {filename = args[0];}
@@ -23,6 +29,7 @@ public class WordSearchKata {
 		catch (Exception e) {
 			System.err.format("Word search file incorrect or could not be opened: '%s'.", filename);
 		    e.printStackTrace();
+		    return null;
 		}
 		//prepare wordsearch to search
 		char[][] lettermap = new char[wordsearch.size()][];
@@ -50,7 +57,7 @@ public class WordSearchKata {
 				if(wordFound == true) {
 					break;
 				}
-			}
+			} 
 			System.out.print(wordlist[j] + ": ");
 			for (int d = 0; d < findings.get(wordlist[j]).size(); d++) {
 				System.out.print("("+ findings.get(wordlist[j]).get(d).getY()+ "," + findings.get(wordlist[j]).get(d).getX() + ")");
@@ -60,6 +67,7 @@ public class WordSearchKata {
 			}
 			System.out.println();
 		}
+		return out.toString();
 	}
 	//find the rest of the word from a certain index
 	public static List<charTuple> findWord(char[][] lettermap, String word, int index, int x, int y, List<charTuple> list){
